bb8cdfa15332164a94105968622463f6f2913e09
Ruben Ortiz <rubend.ortizl@gmail.com>
2023-05-14 18:33:50 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const API_KEY = "aacc24b880a3ab4feda7917bf9adca90";
+
+const form = document.querySelector(".search form");
+const inputCity = document.querySelector(".search input");
+const info = document.querySelector(".info .info-container");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputVal = inputCity.value;
+
+  // agregar coso para que se pueda elegir entre metrico o imperial
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${API_KEY}&unints=metric`;
+
+  console.log(url);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const { name, main, weather, sys } = data;
+
+      const city = document.createElement("div");
+      // city.classList.add("something")
+
+      const weatherMarkUp = `
+        <h2>${name}</h2>
+        <p>${sys.country}</p>
+        <p>${Math.round(main.temp)}C</p>
+        <p>${weather[0]["description"]}</p>
+      `;
+
+      city.innerHTML = weatherMarkUp;
+
+      info.appendChild(city);
+    })
+    .catch(() => {
+      console.log("AAAGH");
+    });
+
+  form.reset();
+  inputCity.focus();
+});
