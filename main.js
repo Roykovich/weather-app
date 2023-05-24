06b42b730a5992c6a9c241d8a58ffa783c4ebee0
Ruben Ortiz <rubend.ortizl@gmail.com>
2023-05-24 00:48:52 -0400

    fix queryselectors and add a new one for something later

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-const API_KEY = "";
+const API_KEY = "3L6JVUZFBDJ8X2L8HNU25Q3XP";
 
-const form = document.querySelector(".search form");
-const inputCity = document.querySelector(".search input");
-const info = document.querySelector(".info");
+const $ = (selector) => document.querySelector(selector);
+
+const headerTitle = $("header h1");
+const form = $(".search form");
+const inputCity = $(".search input");
+const info = $(".info");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -11,8 +14,6 @@ form.addEventListener("submit", (e) => {
   const date = new Date();
 
   const { yesterday: dayBefore, nextDays: fiveDaysAfter } = formatDate(date);
-
-  console.log(dayBefore, fiveDaysAfter);
 
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${inputVal}/${dayBefore}/${fiveDaysAfter}?unitGroup=metric&key=${API_KEY}`;
 
@@ -34,48 +35,49 @@ form.addEventListener("submit", (e) => {
       // En chance of rain agregar una posibilidad de que agarre el preciptype y dependiendo de que sea decir precipitacion de lluvia o nevada
       const weatherMarkUp = `
         <h2 class="city-title">${resolvedAddress}</h2>
-        <div class="current-temperature-container">
-          <h1 class="current-temperature">
-            ${Math.round(
-              today["temp"]
-            )}°<span class="current-unit-system"></span>
-          </h1>
-          <div class="condition-container">
-            <p class="condition">${today["conditions"]}</p> 
-            <p class="temps">${Math.round(today["tempmax"])}°
-              <span class="slash">/</span>${Math.round(today["tempmin"])}°
-            </p>
-          </div>
-        </div>
-        <div class="detailed-information-container">
-          <p class="detailed-information-description">${
-            today["description"]
-          }</p>
-          <div class="detailed-information-columns">
-            <div class="detailed-column">
-              <p>Feels like: <span class="detailed-value">${Math.round(
-                today["feelslike"]
-              )}°</></p>
-              <p>Humidity: <span class="detailed-value">${Math.round(
-                today["humidity"]
-              )}%</></p>
-              <p>Pressure: <span class="detailed-value">${Math.round(
-                today["pressure"]
-              )}mbar</></p>
-              <p>Chance of Precip: <span class="detailed-value">${
-                today["precipprob"]
-              }%</></p>
+        <div class="city-temperature-container">
+          <div class="current-temperature-container">
+            <h1 class="current-temperature">
+              ${Math.round(
+                today["temp"]
+              )}°<span class="current-unit-system"></span>
+            </h1>
+            <div class="condition-container">
+              <p class="condition">${today["conditions"]}</p> 
+              <p class="temps">${Math.round(today["tempmax"])}°
+                <span class="slash">/</span>${Math.round(today["tempmin"])}°
+              </p>
             </div>
-            <div class="detailed-column">
-              <p>Sunrise: <span class="detailed-value">${today["sunrise"].slice(
-                0,
-                -3
-              )}</></p>
-              <p>Sunset: <span class="detailed-value">${today["sunset"].slice(
-                0,
-                -3
-              )}</></p>
-              <p>UV: <span class="detailed-value">${today["uvindex"]}</></p>
+          </div>
+          <div class="detailed-information-container">
+            <p class="detailed-information-description">${
+              today["description"]
+            }</p>
+            <div class="detailed-information-columns">
+              <div class="detailed-column">
+                <p>Feels like: <span class="detailed-value">${Math.round(
+                  today["feelslike"]
+                )}°</></p>
+                <p>Humidity: <span class="detailed-value">${Math.round(
+                  today["humidity"]
+                )}%</></p>
+                <p>Pressure: <span class="detailed-value">${Math.round(
+                  today["pressure"]
+                )}mbar</></p>
+                <p>Chance of Precip: <span class="detailed-value">${
+                  today["precipprob"]
+                }%</></p>
+              </div>
+              <div class="detailed-column">
+                <p>Sunrise: <span class="detailed-value">${today[
+                  "sunrise"
+                ].slice(0, -3)}</></p>
+                <p>Sunset: <span class="detailed-value">${today["sunset"].slice(
+                  0,
+                  -3
+                )}</></p>
+                <p>UV: <span class="detailed-value">${today["uvindex"]}</></p>
+              </div>
             </div>
           </div>
         </div>
